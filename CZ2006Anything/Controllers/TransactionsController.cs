d87dd02abdffd52974ac94ec8d6bc33c315d6a5a
Zheng Tao <zhengtaolim@gmail.com>
2019-09-22 02:54:05 +0800

    Created Transactions Detail
    aka generate qr code page

---
+++ CZ2006Anything/Controllers/TransactionsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CZ2006Anything.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        // GET: Transactions
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Detail(int id)
+        {
+            return View(id);
+        }
+    }
+}
